a3af84a69f774e68a80359bf50056385eeebe033
Сергей Ежов <serga.ezhov.5@gmail.com>
2022-03-16 00:14:25 +0600

    feat: add createComponent

---
+++ src/components/createComponent/CreateComponent.tsx
@@ -0,0 +1,48 @@
+import React, { FC, useState } from "react";
+import { useInput } from "../../hooks/useInput";
+import { ButtonCreate, FormCreate } from './CreateComponent.style'
+
+type props = {
+  type: 'column' | 'card';
+  handleCreateComponent: (value: string) => void;
+}
+
+export const CreateComponent: FC<props> = ({ type, handleCreateComponent }) => {
+  const [isCreate, setIsCreate] = useState<Boolean>(false);
+  const { bind, value, clear } = useInput();
+
+  const createComponent = () => {
+    handleCreateComponent(value())
+    exit()
+  }
+
+  const exit = () => {
+    clear()
+    setIsCreate(false)
+  }
+
+  return (
+    <>
+      {
+        !isCreate
+          ?
+          <ButtonCreate type='button' onClick={() => setIsCreate(true)}>
+            &#10010; Добавить {type === 'column'
+              ? 'колонку'
+              : 'карточку'}
+          </ButtonCreate>
+          :
+          <FormCreate>
+            <input type="text" {...bind} />
+
+            <button type='submit' onClick={() => createComponent()}>
+              Создать {type === 'column'
+                ? 'колонку'
+                : 'карточку'}
+            </button>
+            <button onClick={() => exit()}>&#10006;</button>
+          </FormCreate>
+      }
+    </>
+  )
+}
