846894e0eb04414a6e404e36521248e456a70984
Сергей Ежов <serga.ezhov.5@gmail.com>
2022-03-15 12:34:58 +0600

    feat: add base type in types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,20 +1,24 @@
-export type Column = {
+export type Column = BaseType & {
     name: string;
     cards?: Card[];
 }
 
-export type Card = {
+export type Card = BaseType & {
     name: string;
     author: User;
     content?: string;
     comments?: Comment[];
 }
 
-export type Comment = {
+export type Comment = BaseType & {
     body: string;
     author: User;
 }
 
-export type User = {
+export type User = BaseType & {
     name: string;
+}
+
+export type BaseType = {
+    id: string;
 }
