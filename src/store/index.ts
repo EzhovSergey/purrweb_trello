8f6e985c6aa93bc3367d25f48b3b328480d3272a
Сергей Ежов <serga.ezhov.5@gmail.com>
2022-03-30 14:28:57 +0600

    feat: redux persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import { columnsSlice, cardsSlice, commentsSlice, userSlice } from './ducks';
 
-export const store = configureStore({
-  reducer: {
-    columns: columnsSlice,
-    cards: cardsSlice,
-    comments: commentsSlice,
-    user: userSlice
-  },
+const rootReducer = combineReducers({
+  columns: columnsSlice,
+  cards: cardsSlice,
+  comments: commentsSlice,
+  user: userSlice
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const reducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persistor = persistStore(store);
+export default store;
 
 export * from './ducks';
 
